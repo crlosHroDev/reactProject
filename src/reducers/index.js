291ac32b2bc71758e2f43748e7ad2dba7ba2dc2e
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-13 14:38:19 -0500

    Estados de carga

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import data from './data';
 import moda from './modal';
+import isLoading from './is-loading'
 
 //import {combineReducers} from 'redux';
 import {combineReducers} from 'redux-inmutable';
 
 const rootReducer=combineReducers({
   data,
-  modal
+  modal,
+  isLoading
 })
 
 export default rootReducer;
