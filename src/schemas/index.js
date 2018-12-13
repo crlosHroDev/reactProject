fe992d00e60ce39f769397cc6388290227b46ae6
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-13 09:51:27 -0500

    Normalizando datos

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,11 +2,17 @@ import api from '../api.json';
 
 import {normalize,schema} from 'normalizr';
 
-const media=new schema.Entity() 
+//const media=new schema.Entity(key como quiero que se llame,definicion esquema, opciones para perzonalizar)
+const media=new schema.Entity('media',{},{ 
+  idAttribute:'id',
+  processStrategy:(value,parent,key)=>({...value,category:parent.id})
+})  
 
-const category=new schema.Entity()
+const category=new schema.Entity('categories',{
+  playlist:new schema.Array(media)
+})
 
-const categories= 
+const categories= {categories:new schema.Array(category)}
 
 const normalizeData=normalize(api,categories);
 
