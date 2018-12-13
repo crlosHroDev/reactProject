345a64be73797f567c21e3dfe8e0a57efa517779
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-13 12:46:45 -0500

    Creadores de acciones

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -8,6 +8,9 @@ import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
 import {connect} from 'react-redux';
 import {List as list} from 'inmutable';
+import {openModal,close} from '../actions/index';
+
+
 class Home extends Component {
   /*state = {
     modalVisible: false,
@@ -17,20 +20,13 @@ class Home extends Component {
       modalVisible: true,
       media
     })*/
-      this.props.dispatch({
-        type:'OPEN_MODAL',
-        payload:{
-          mediaId:id
-        }
-      })
+      this.props.dispatch(openModal(id))
   }
   handleCloseModal = (event) => {
     /*this.setState({
       modalVisible: false,
     })*/
-    this.props.dispatch({
-      type:'CLOSE_MODAL'
-    })
+    this.props.dispatch(closeModal())
   }
   render() {
     return (
