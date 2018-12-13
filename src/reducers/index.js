534ac3023c78d16e2c0c8d4cabbc01fb221051f4
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-13 11:40:31 -0500

    Usando datos inmutables en Platzi Video

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import data from './data';
 import moda from './modal';
 
-import {combineReducers} from 'redux';
+//import {combineReducers} from 'redux';
+import {combineReducers} from 'redux-inmutable';
 
 const rootReducer=combineReducers({
   data,
